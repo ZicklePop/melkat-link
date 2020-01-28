a76b1c90d6865ff132fe394394fd8a305966b326
melkat <318003+ZicklePop@users.noreply.github.com>
2020-01-27 20:43:10 -0800

    more tests

---
+++ __tests__/layout.test.js
@@ -0,0 +1,20 @@
+/** @jsx h */
+import { h } from 'preact'
+import render from 'preact-render-to-string'
+import Layout from '../components/layout'
+
+const MOCK = {
+  title: 'Rae the Doe',
+  description: 'A webcomic about a gay disaster, a punk skunk, and lots and lots of puns. Created by Olive Rae Brinker. Updates Monday, Wednesday and Friday.',
+}
+
+describe('component - layout', () => {
+  it('should render', () => {
+    const component = render(<Layout />)
+    expect(component).toMatchSnapshot()
+  })
+  it('should render with props', () => {
+    const component = render(<Layout {...MOCK}><b>{'#content'}</b></Layout>)
+    expect(component).toMatchSnapshot()
+  })
+})
